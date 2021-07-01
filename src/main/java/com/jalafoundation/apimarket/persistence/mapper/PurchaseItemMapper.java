b78550afe687450d85d43aff4890056a9efb9876
Rolando Mamani Salas <Rolando.Mamani@fundacion-jala.org>
2021-07-01 06:56:20 -0400

    creando los mapstruct para mapear aun no esta completado por que me falta mas investigacion de como funcionan

---
+++ src/main/java/com/jalafoundation/apimarket/persistence/mapper/PurchaseItemMapper.java
@@ -0,0 +1,12 @@
+package com.jalafoundation.apimarket.persistence.mapper;
+
+import com.jalafoundation.apimarket.domain.service.PurchaseItem;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring") // esta clase sirve parea hacer el MapStruct
+public interface PurchaseItemMapper {
+
+  //  PurchaseItem toPurchaseItem(ComprasProduct product){
+
+   // }
+}
